43a6e89149e0d64912a54ff5b935240e902f0f11
valentinech <valentinech@yandex.ru>
2018-11-05 10:46:02 +0400

    Initial Commit

---
+++ CacheSystem.Domain/Entities/Employee.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CacheSystem.Domain.Entities
+{
+    public class Employee
+    {
+    }
+}
